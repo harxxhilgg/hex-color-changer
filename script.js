6ba8a97b6bbf1eb28b7c65740c79e06fd6eabe1f
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-04-08 00:16:57 +0530

    sed

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputValue = document.getElementById('myInput');
+
+inputValue.addEventListener("keypress", function(event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+
+        document.getElementById('myBtn').click();
+
+        console.log();
+    }
+})
